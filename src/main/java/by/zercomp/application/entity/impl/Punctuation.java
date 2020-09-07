188268a895df6079dce38ae40460e17621d0dabe
LeviathanZC <leviathan.zercomp@gmail.com>
2020-09-07 13:40:29 +0300

    Impl Punctuation and Sentence

--- src/main/java/by/zercomp/application/entity/impl/Punctuation.java
+++ src/main/java/by/zercomp/application/entity/impl/Punctuation.java
@@ -1,4 +1,52 @@
 package by.zercomp.application.entity.impl;
 
-public class Punctuation {
+import by.zercomp.application.entity.SentenceItem;
+import by.zercomp.application.entity.TextItem;
+
+public class Punctuation implements SentenceItem {
+    private Character character;
+
+    public Punctuation(Character character) {
+        this.character = character;
+    }
+
+    public Character getCharacter() {
+        return character;
+    }
+
+    public void setCharacter(Character character) {
+        this.character = character;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || this.getClass() != o.getClass()) {
+            return false;
+        }
+
+        Punctuation that = (Punctuation) o;
+
+        return getCharacter() != null ? getCharacter().equals(that.getCharacter()) : that.getCharacter() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return getCharacter() != null ? getCharacter().hashCode() : 0;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append(character);
+        builder.append(" ");
+        return builder.toString();
+    }
+
+    @Override
+    public void setTextItem(TextItem textItem) {
+        character = textItem.toString().charAt(0);
+    }
 }
--- src/main/java/by/zercomp/application/entity/impl/Sentence.java
+++ src/main/java/by/zercomp/application/entity/impl/Sentence.java
@@ -1,4 +1,50 @@
 package by.zercomp.application.entity.impl;
 
-public class Sentence {
+import by.zercomp.application.entity.SentenceItem;
+import by.zercomp.application.entity.TextItem;
+import com.sun.xml.internal.bind.v2.TODO;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Sentence implements TextItem {
+    private List<SentenceItem> sentence = new LinkedList<>();
+
+    @Override
+    public void setTextItem(TextItem textItem) {
+        sentence.add((SentenceItem) textItem);
+    }
+
+    public List<SentenceItem> getSentence() {
+        return sentence;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || this.getClass() != o.getClass()) {
+            return false;
+        }
+
+        Sentence sentence = (Sentence) o;
+
+        return this.sentence.equals(sentence.getSentence());
+    }
+
+    @Override
+    public int hashCode() {
+        return this.sentence.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        // TODO: 7.09.20  
+        StringBuilder builder = new StringBuilder();
+        for (SentenceItem item : sentence) {
+            builder.append(item);
+        }
+        return builder.toString();
+    }
 }
